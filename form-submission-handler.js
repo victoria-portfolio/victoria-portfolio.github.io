a54d262ef62f5952f618f6d7fd7595337ebe7293
justjeff <hello@jeffsherry.com>
2018-10-05 16:52:20 -0500

    .

--- form-submission-handler.js
+++ form-submission-handler.js
@@ -84,7 +84,7 @@
             xhr.onreadystatechange = function () {
                 console.log(xhr.status, xhr.statusText);
                 console.log(xhr.responseText);
-                var formElements = form.querySelector(".form-elements")
+                var formElements = form.querySelector(".form")
                 if (formElements) {
                     formElements.style.display = "none"; // hide form
                 }
